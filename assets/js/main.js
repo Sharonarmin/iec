8f8659832871b1d276805c89c1007b4112d82054
sharon <sharonarmin84@gmail.com>
2024-01-11 09:38:56 +0530

    'jjj'

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,21 @@ function makeSticky(){
 
 // aos
 AOS.init();
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+      },
+  });
